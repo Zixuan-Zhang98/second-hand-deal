8a10e21e41b4cace0f20676a6c67504de989671b
chenghaonyew <nyewch0404@gmail.com>
2020-01-08 13:34:31 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,172 @@
+export default [
+  {
+    sys: {
+      id: "1"
+    },
+    fields: {
+      name: "CSCI 2021",
+      category: "textbook",
+      price: 25,
+      seller: "Sophia",
+      condition: 5,
+      Location: "Quad",
+      Contact: "0111111111",
+      reviewed: 36,
+      lastEditing: "02/02/2016 1:38 PM",
+      description:
+        " A modern personal portfolio webpage that built for showcasing my projects and resume.",
+      images: [
+        {
+          fields: {
+            file: {
+              url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+            }
+          }
+        },
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "1"
+    },
+    fields: {
+      name: "CSCI 2021",
+      category: "textbook",
+      price: 25,
+      seller: "Sophia",
+      condition: 5,
+      Location: "Quad",
+      Contact: "0111111111",
+      reviewed: 36,
+      lastEditing: "02/02/2016 1:38 PM",
+      description:
+        " A modern personal portfolio webpage that built for showcasing my projects and resume.",
+      images: [
+        {
+          fields: {
+            file: {
+              url: "https://s.ftcdn.net/v2013/pics/all/curated/RKyaEDwp8J7JKeZWQPuOVWvkUjGQfpCx_cover_580.jpg?r=1a0fc22192d0c808b8bb2b9bcfbf4a45b1793687"
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: "https://s.ftcdn.net/v2013/pics/all/curated/RKyaEDwp8J7JKeZWQPuOVWvkUjGQfpCx_cover_580.jpg?r=1a0fc22192d0c808b8bb2b9bcfbf4a45b1793687"
+            }
+          }
+        },
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "1"
+    },
+    fields: {
+      name: "CSCI 2021",
+      category: "textbook",
+      price: 25,
+      seller: "Sophia",
+      condition: 5,
+      Location: "Quad",
+      Contact: "0111111111",
+      reviewed: 36,
+      lastEditing: "02/02/2016 1:38 PM",
+      description:
+        " A modern personal portfolio webpage that built for showcasing my projects and resume.",
+      images: [
+        {
+          fields: {
+            file: {
+              url: ""
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: ""
+            }
+          }
+        },
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "1"
+    },
+    fields: {
+      name: "CSCI 2021",
+      category: "textbook",
+      price: 25,
+      seller: "Sophia",
+      condition: 5,
+      Location: "Quad",
+      Contact: "0111111111",
+      reviewed: 36,
+      lastEditing: "02/02/2016 1:38 PM",
+      description:
+        " A modern personal portfolio webpage that built for showcasing my projects and resume.",
+      images: [
+        {
+          fields: {
+            file: {
+              url: "https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-260nw-721703848.jpg"
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: "https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-260nw-721703848.jpg"
+            }
+          }
+        },
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "1"
+    },
+    fields: {
+      name: "CSCI 2021",
+      category: "textbook",
+      price: 25,
+      seller: "Sophia",
+      condition: 5,
+      Location: "Quad",
+      Contact: "0111111111",
+      reviewed: 36,
+      lastEditing: "02/02/2016 1:38 PM",
+      description:
+        " A modern personal portfolio webpage that built for showcasing my projects and resume.",
+      images: [
+        {
+          fields: {
+            file: {
+              url: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg"
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg"
+            }
+          }
+        },
+      ]
+    }
+  },
+];
